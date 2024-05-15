ec084dab3e021b5caf1adf5fc508d5258996485b
Aldo Ortega González <aeledeo.9@gmail.com>
2024-05-14 19:39:07 -0600

    Update tri.ino

--- triangulo/tri.ino
+++ triangulo/tri.ino
@@ -3,13 +3,14 @@
 Servo servo_9;
 Servo servo_10;
 Servo servo_11;
-int pos = 0;
+int posx = 0;
+int posy=0;
 
 void setup()
 {
-  servo_9.attach(9, 500, 2500);
-  servo_10.attach(10,500,2500);
-  servo_11.attach(10.500.2500)
+  servo_9.attach(9);
+  servo_10.attach(10);
+  //servo_11.attach(11);
 }
 
 void loop()
@@ -18,18 +19,18 @@ void loop()
 }
 
 void triangulo()
-{// sweep the servo from 0 to 180 degrees in steps
-  // of 1 degrees
-  for (pos = 0; pos <= 180; pos += 1) {
-    // tell servo to go to position in variable 'pos'
-    servo_9.write(pos);
-    // wait 15 ms for servo to reach the position
-    delay(100); // Wait for 100 millisecond(s)
+{
+ for (posx = 180; posx<= 360; posx += 1) {
+    
+    servo_9.write(posx);
+    delay(5); 
   }
-  for (pos = 180; pos >= 0; pos -= 1) {
-    // tell servo to go to position in variable 'pos'
-    servo_9.write(pos);
-    // wait 15 ms for servo to reach the position
-    delay(5); // Wait for 5 millisecond(s)
+  
+ for (posx = 0; posx<= 180; posx += 1) {
+    
+    servo_9.write(posx);
+   	servo_10.write(posx);
+   delay(100);
   }
+  
 }
