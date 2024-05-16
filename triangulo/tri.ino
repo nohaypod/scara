f4c7a3d0acc9a529ecc589564484d355b206c13e
Aldo Ortega González <aeledeo.9@gmail.com>
2024-05-15 20:42:11 -0600

    Update tri.ino

--- triangulo/tri.ino
+++ triangulo/tri.ino
@@ -1,40 +1,4 @@
 #include <Servo.h>
-
-Servo servo_9;
-Servo servo_10;
-Servo servo_11;
-int posx = 0;
-int posy=0;
-
-void setup()
-{
-  servo_9.attach(9);
-  servo_10.attach(10);
-  //servo_11.attach(11);
-}
-
-void loop()
-{
-  triangulo();
-}
-
-void triangulo()
-{
- for (posx = 180; posx<= 360; posx += 1) {
-    
-    servo_9.write(posx);
-    delay(5); 
-  }
-  
- for (posx = 0; posx<= 180; posx += 1) {
-    
-    servo_9.write(posx);
-   	servo_10.write(posx);
-   delay(100);
-  }
-  
-}
-#include <Servo.h>
 int pos = 180;
 Servo servo_9;
 Servo servo_10;
